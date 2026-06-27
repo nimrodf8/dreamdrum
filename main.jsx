9749371c5d9ca839134c2953564b73f0dfba589f
Nimrod <nimrodf8@email.com>
2026-06-27 13:37:18 +0300

    Move gate files into src and add Supabase

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AuthGate from "./AuthGate.jsx";
-
-createRoot(document.getElementById("root")).render(<AuthGate />);
